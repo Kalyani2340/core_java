d2729ad9e139bebce3fc9de5ec40a8ae6dd4535f
Kalyani2340 <99206707+Kalyani2340@users.noreply.github.com>
2023-12-01 16:10:02 +0530

    First Program

---
+++ src/com/radical/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.radical.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+int a,b,c;
+a=3;
+b=5;
+c=a+b;
+System.out.println(c);
+	}
+
+}
