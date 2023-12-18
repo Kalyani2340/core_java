f4bbf9ea91416e8f91ca01bb0e0ee39df67caa76
Kalyani2340 <99206707+Kalyani2340@users.noreply.github.com>
2023-12-18 20:03:29 +0530

    formating added

--- src/com/radical/test/HelloWorld.java
+++ src/com/radical/test/HelloWorld.java
@@ -4,12 +4,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World");
-int a,b,c;
-a=3;
-b=5;
-c=a+b;
-System.out.println(c);
+		System.out.println("Hello World");
+		int a, b, c;
+		a = 3;
+		b = 5;
+		c = a + b;
+		System.out.println(c);
 	}
 
 }
